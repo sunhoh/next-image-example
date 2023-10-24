bbc94a8c3279e1aee0b6af397839638dee15d0fb
sunhoh <dltjsgho94@gmail.com>
2023-10-24 13:24:02 +0900

    change next/image library to sharp

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    domains: [],
+  },
+  
     async redirects() {
         return [
           {
